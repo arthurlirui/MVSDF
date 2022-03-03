24d738a846cb02f74bf49eb1341e7550029e19f0
arthurlirui <arthurlirui@gmail.com>
2022-03-03 23:45:07 +0300

    update

--- exp_runner.py
+++ exp_runner.py
@@ -48,5 +48,21 @@ def train_all():
 
 
 if __name__ == '__main__':
-    print('test')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='fill_in_data_dir')
+    parser.add_argument('--batch_size', type=int, default=8, help='input batch size')
+    parser.add_argument('--nepoch', type=int, default=1800, help='number of epochs to train for')
+    parser.add_argument('--conf', type=str, default='./confs/mvsdf_dtu.conf')
+    parser.add_argument('--expname', type=str, default='test')
+    parser.add_argument('--gpu', type=str, default='auto', help='GPU to use [default: GPU auto]')
+    parser.add_argument('--is_continue', default=False, action="store_true",
+                        help='If set, indicates continuing from a previous run.')
+    parser.add_argument('--timestamp', default='latest', type=str,
+                        help='The timestamp of the run to be used in case of continuing from a previous run.')
+    parser.add_argument('--checkpoint', default='latest', type=str,
+                        help='The checkpoint epoch number of the run to be used in case of continuing from a previous run.')
+    # parser.add_argument('--train_cameras', default=False, action="store_true", help='If set, optimizing also camera location.')
+
+    opt = parser.parse_args()
+    print(opt)
 
